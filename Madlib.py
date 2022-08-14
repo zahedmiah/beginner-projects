15815b36a5f757451dfe60bdeed35daff7286339
zahedmiah <110930755+zahedmiah@users.noreply.github.com>
2022-08-14 16:13:27 +0100

    Add files via upload
    Madlib practice, not the most complex of coding - just baby-steps.

---
+++ Madlib.py
@@ -0,0 +1,8 @@
+#madlib practice
+ 
+adj1 = input("adj ")
+adj2 = input("adj ")
+verb1 = input("verb ")
+
+madlib = (f"I love coding, because it is very {adj1} and {adj2}. It also makes me think and therefore increases my {verb1}")
+print(madlib)
