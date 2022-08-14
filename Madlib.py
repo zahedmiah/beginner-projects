5f4e68506e3f7fc161c82076ce0983bb0c556a33
zahedmiah <110930755+zahedmiah@users.noreply.github.com>
2022-08-14 16:18:23 +0100

    Update Madlib.py

--- Madlib.py
+++ Madlib.py
@@ -1,8 +1,10 @@
 #madlib practice
+#practiced learning from a Youtube Video by FreeCodeCamp
+#learnt a few important concepts, such as input function, and f string
  
 adj1 = input("adj ")
 adj2 = input("adj ")
-verb1 = input("verb ")
+noun1 = input("noun ")
 
-madlib = (f"I love coding, because it is very {adj1} and {adj2}. It also makes me think and therefore increases my {verb1}")
+madlib = (f"I love coding, because it is very {adj1} and {adj2}. It also makes me think and therefore increases my {noun1}")
 print(madlib)
